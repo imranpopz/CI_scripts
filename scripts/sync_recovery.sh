a2e95484294844da2fa88cf486ced15a496b688c
imranpopz <imranpopz32@gmail.com>
2023-02-12 18:34:33 +0530

    Update sync_recovery.sh

--- scripts/sync_recovery.sh
+++ scripts/sync_recovery.sh
@@ -8,7 +8,7 @@ git config --global user.name "$USERNAME";
 git config --global user.email "$USERMAIL";
 git config --global color.ui auto
 
-repo init -u $CR_REPO -b $CR_BRANCH;
+repo init -u --depth=1 $CR_REPO -b $CR_BRANCH;
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags;
 
 git clone $DT_REPO -b $DT_BRANCH "device/$OEM/$CODENAME";
