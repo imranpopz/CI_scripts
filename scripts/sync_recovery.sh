6c447d6d9ca9edf09e955c0a006cb87bb2ef29e2
imranpopz <imranpopz32@gmail.com>
2023-02-12 17:10:49 +0530

    Set color ui by default

--- scripts/sync_recovery.sh
+++ scripts/sync_recovery.sh
@@ -3,10 +3,12 @@
 cd ~;
 mkdir $CR_NAME;
 cd $CR_NAME;
+
 git config --global user.name "$USERNAME";
 git config --global user.email "$USERMAIL";
-repo init -u $CR_REPO -b $CR_BRANCH;
+git config --global color.ui auto
 
+repo init -u $CR_REPO -b $CR_BRANCH;
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags;
 
 git clone $DT_REPO -b $DT_BRANCH "device/$OEM/$CODENAME";
