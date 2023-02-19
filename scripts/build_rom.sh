b5e9c38df16c2a5d0430ba149d4120dbe84e4c1a
imranpopz <imranpopz32@gmail.com>
2023-02-19 18:43:02 +0530

    update scripts

--- scripts/build_rom.sh
+++ scripts/build_rom.sh
@@ -7,9 +7,11 @@ source build/envsetup.sh;
 
 cd device/$OEM/$CODENAME;
 mv aosp_sweet.mk $PRODUCT_NAME_$CODENAME.mk;
-sed -i 's/aosp/$PRODUCT_NAME/g'$PRODUCT_NAME_$CODENAME.mk;
+sed -i 's/aosp/$PRODUCT_NAME/g' $PRODUCT_NAME_$CODENAME.mk;
+sed -i 's/aosp/$PRODUCT_NAME/g' AndroidProducts.mk;
 cd - ;
-lunch $PRODUCT_NAME-$VARIANT
 
+lunch $PRODUCT_NAME-$VARIANT
 mka $TARGET -j$(nproc --all);
+
 exit 0;
