f92666129c44a71688a51718238a798162029cf0
imranpopz <imranpopz32@gmail.com>
2023-02-19 19:23:38 +0530

    update scripts

--- scripts/build_rom.sh
+++ scripts/build_rom.sh
@@ -5,11 +5,6 @@ cd tmp
 export ALLOW_MISSING_DEPENDENCIES=true
 source build/envsetup.sh;
 
-cd device/$OEM/$CODENAME;
-mv aosp_sweet.mk $PRODUCT_NAME_$CODENAME.mk;
-sed -i 's/aosp/$PRODUCT_NAME/g' $PRODUCT_NAME_$CODENAME.mk;
-sed -i 's/aosp/$PRODUCT_NAME/g' AndroidProducts.mk;
-cd - ;
 
 lunch $PRODUCT_NAME-$VARIANT
 mka $TARGET -j$(nproc --all);
--- scripts/sync_rom.sh
+++ scripts/sync_rom.sh
@@ -10,15 +10,20 @@ git config --global color.ui auto
 
 # Clone GREEN source
 git clone $DEVICE_REPO -b $DEVICE_BRANCH "device/$OEM/$CODENAME";
+cd device/$OEM/$CODENAME;
+mv aosp_sweet.mk $PRODUCT_NAME_$CODENAME.mk;
+sed -i 's/aosp/$PRODUCT_NAME/g' $PRODUCT_NAME_$CODENAME.mk;
+sed -i 's/aosp/$PRODUCT_NAME/g' AndroidProducts.mk;
+cd - ;
 ls device/$OEM/$CODENAME
 #git clone $VENDOR_REPO -b $VENDOR_BRANCH "vendor/$OEM/$CODENAME";
 #git clone $KERNEL_REPO -b $KERNEL_BRANCH "kernel/$OEM/$CODENAME";
 
 
 # Initialize RED source
-repo init -u $ROM_REPO -b $ROM_BRANCH --depth=1;
+#repo init -u $ROM_REPO -b $ROM_BRANCH --depth=1;
 
 # Sync RED source
-repo sync
+#repo sync
 
 exit 0;
