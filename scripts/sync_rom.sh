1639ed19f5ed815817e9a35021828f60370d5cb6
imranpopz <imranpopz32@gmail.com>
2023-02-19 17:16:29 +0530

    update sync script

--- scripts/sync_rom.sh
+++ scripts/sync_rom.sh
@@ -7,17 +7,14 @@ cd tmp;
 
 # Clone GREEN source
 git clone $DEVICE_REPO -b $DEVICE_BRANCH "device/$OEM/$CODENAME";
+#git clone $VENDOR_REPO -b $VENDOR_BRANCH "vendor/$OEM/$CODENAME";
+#git clone $KERNEL_REPO -b $KERNEL_BRANCH "kernel/$OEM/$CODENAME";
 
-git clone $VENDOR_REPO -b $VENDOR_BRANCH "vendor/$OEM/$CODENAME";
-
-git clone $KERNEL_REPO -b $KERNEL_BRANCH "kernel/$OEM/$CODENAME";
-
-git clone $FIRMWARE_REPO -b $FIRMWARE_BRANCH "vendor/$OEM/firmware"
 
 # Initialize RED source
-repo init -u $ROM_REPO -b $ROM_BRANCH --depth=1;
+#repo init -u $ROM_REPO -b $ROM_BRANCH --depth=1;
 
 # Sync RED source
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags;
+#repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags;
 
 exit 0;
