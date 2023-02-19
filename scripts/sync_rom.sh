23d6a42d53d43a9a63ec6e8b3f780b8c9daecf47
imranpopz <imranpopz32@gmail.com>
2023-02-19 14:10:24 +0530

    update sync script

--- scripts/sync_rom.sh
+++ scripts/sync_rom.sh
@@ -15,7 +15,7 @@ git clone $KERNEL_REPO -b $KERNEL_BRANCH "kernel/$OEM/$CODENAME";
 git clone $FIRMWARE_REPO -b $FIRMWARE_BRANCH "vendor/$OEM/firmware"
 
 # Initialize RED source
-repo init -u $ROM_REPO -b $ROM_BRANCH;
+repo init -u $ROM_REPO -b $ROM_BRANCH --depth=1;
 
 # Sync RED source
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags;
