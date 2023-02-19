85325b4e96f73a20ac02e0ddd905ad5c5a98a3e3
imranpopz <imranpopz32@gmail.com>
2023-02-19 18:40:19 +0530

    update scripts

--- scripts/build_rom.sh
+++ scripts/build_rom.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 cd ~
-cd ${ROM_NAME}
+cd tmp
 export ALLOW_MISSING_DEPENDENCIES=true
 source build/envsetup.sh;
 
+cd device/$OEM/$CODENAME;
+mv aosp_sweet.mk $PRODUCT_NAME_$CODENAME.mk;
+sed -i 's/aosp/$PRODUCT_NAME/g'$PRODUCT_NAME_$CODENAME.mk;
+cd - ;
 lunch $PRODUCT_NAME-$VARIANT
 
 mka $TARGET -j$(nproc --all);
