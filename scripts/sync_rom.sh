77a3dfe1c3c9091568f222374a731cd7cd4e1608
imranpopz <imranpopz32@gmail.com>
2023-02-20 08:14:28 +0530

    update scripts

--- scripts/sync_rom.sh
+++ scripts/sync_rom.sh
@@ -28,10 +28,11 @@ cd -
 
 
 # Initialize RED source
-repo init -u $ROM_REPO -b $ROM_BRANCH --depth=1;
+repo init -u $ROM_REPO -b $ROM_BRANCH --depth=1 -g default,-device,-mips,-darwin,-notdefault;
 
 # Sync RED source
-repo sync
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 git clone -b Tiramisu https://github.com/Project-Elixir/hardware_xiaomi.git hardware/xiaomi
+
 exit 0;
