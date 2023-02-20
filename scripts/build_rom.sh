8e9e70540a2a8793126514c5fc5939c16f9a4336
imranpopz <imranpopz32@gmail.com>
2023-02-20 09:34:36 +0530

    Update build_rom.sh

--- scripts/build_rom.sh
+++ scripts/build_rom.sh
@@ -6,7 +6,7 @@ export ALLOW_MISSING_DEPENDENCIES=true
 source build/envsetup.sh;
 
 
-lunch $PRODUCTNAME-$VARIANT
+lunch ${PRODUCTNAME}_${CODENAME}-${VARIANT}
 mka $TARGET -j$(nproc --all);
 
 exit 0;
